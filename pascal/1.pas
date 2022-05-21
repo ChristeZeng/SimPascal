8d42d70be6fa7b032119d35ca27a4c1eeb6008ff
QS-Yang <1094075179@qq.com>
2022-05-21 23:26:16 +0800

    test code

--- pascal/1.pas
+++ pascal/1.pas
@@ -2,12 +2,14 @@ program QSort;
   const
     MaxN = 100;
   var
+    validCourse : array[0..99] of integer;
     crediet : array[0..99] of integer;
     grade : array[0..99] of char;
     require : array[0..99] of array[0..99] of integer;
     n : integer;
     num : integer;
     idx : integer;
+    validIdx : integer;
     c : char;
     ch : char;
     binary : char;
@@ -23,13 +25,17 @@ program QSort;
     getCrediet := 0;
     remainCredite := 0;
     gradeTmp := 0;
+    validIdx := 0;
     readln(c);
     while c = 'c' do
     begin
       readln(n);
+      validCourse[validIdx] := n;
+      validIdx := validIdx + 1;
       readln(ch);
       readln(num);
       crediet[n] := num;
+      totalCrediet := totalCrediet + num;
       readln(ch);
       readln(ch);
       write(ch);
@@ -56,19 +62,55 @@ program QSort;
         end;
       end; 
       require[n][idx] := -2;
-      if ch = '|' then 
-      begin
         readln(ch);
         write(ch);
-        if ch <> '|' then 
+        if ch <> '\n' then 
         begin
+          attemptCrediet := attemptCrediet + num;
+          if ch = 'A' then
+          begin
+            getCrediet := getCrediet + num;
+            gradeTmp := gradeTmp + 4 * num;
+          end;
+          if ch = 'B' then
+          begin
+            getCrediet := getCrediet + num;
+            gradeTmp := gradeTmp + 3 * num;
+          end;
+          if ch = 'C' then
+          begin
+            getCrediet := getCrediet + num;
+            gradeTmp := gradeTmp + 2 * num;
+          end;
+          if ch = 'D' then
+          begin
+            getCrediet := getCrediet + num;
+            gradeTmp := gradeTmp + 1 * num;
+          end;
           grade[n] := ch;
-          readln(c);
-          write(c);
+          readln(ch);
+          write(ch);
         end;
-      end;
       
       readln(c);
       if c <> 'c' then writeln('e');
     end;
+  validCourse[validIdx] = -1;
+
+  validIdx := 0;
+  if totalCrediet <> getCrediet then
+  begin
+    while validCourse[validIdx] <> -1 do 
+    begin
+      n := validCourse[validIdx];
+      idx := 0;
+      while require[n][idx] <> -2 do
+      begin
+
+      end;
+      validIdx := validIdx + 1;
+    end;
+  end;
+  
+
   end.
