227227f6b340e0fd224468c94aafe82ccfba92ee
qs <1094075179@qq.com>
2022-05-21 16:47:23 +0800

    test code

--- pascal/1.pas
+++ pascal/1.pas
@@ -2,10 +2,73 @@ program QSort;
   const
     MaxN = 100;
   var
-    a : array[0..2] of array[0..2] of integer;
+    crediet : array[0..99] of integer;
+    grade : array[0..99] of char;
+    require : array[0..99] of array[0..99] of integer;
     n : integer;
+    num : integer;
+    idx : integer;
+    c : char;
+    ch : char;
+    binary : char;
+    totalCrediet : integer;
+    attemptCrediet : integer;
+    getCrediet : integer;
+    remainCredite : integer;
+    gradeTmp : integer;
 
-begin
-  readln(a[0][0]);
-  writeln(a[0][0]);
-end.
+  begin
+    totalCrediet := 0;
+    attemptCrediet := 0;
+    getCrediet := 0;
+    remainCredite := 0;
+    gradeTmp := 0;
+    readln(c);
+    while c = 'c' do
+    begin
+      readln(n);
+      readln(ch);
+      readln(num);
+      crediet[n] := num;
+      readln(ch);
+      readln(ch);
+      write(ch);
+      idx := 0;
+      while ch <> '|' do
+      begin
+        readln(num);
+        write(num);
+        require[n][idx] := num;
+        idx := idx + 1;
+        readln(ch);
+        write(ch);
+        if ch = ',' then
+        begin
+          readln(ch);
+          write(ch);
+        end;
+        if ch = ';' then
+        begin
+          require[n][idx] := -1;
+          idx := idx + 1;
+          readln(ch);
+          write(ch);
+        end;
+      end; 
+      require[n][idx] := -2;
+      if ch = '|' then 
+      begin
+        readln(ch);
+        write(ch);
+        if ch <> '|' then 
+        begin
+          grade[n] := ch;
+          readln(c);
+          write(c);
+        end;
+      end;
+      
+      readln(c);
+      if c <> 'c' then writeln('e');
+    end;
+  end.
